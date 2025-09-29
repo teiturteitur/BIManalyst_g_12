9e1256ff7d43d85154df8aca8ec37af88f6d33eb
Teitur <s201348@student.dtu.dk>
2025-09-29 15:36:07 +0200

    update

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ and checks if it is defined on the correct level in the IFC file.
 
 How to use:
 - Load your IFC file by changing the path for the ifc_file variable
-- Specify the element type you want to check (currently set to IfcDuctSegment)
-- Run main.py
+- Run main.py and follow the instructions in the CLI
 
 
 Future Work:
-- Counter of moved elements and elements with insufficient free height
-- General statistics report
 - GUI?
 
 
@@ -31,6 +28,7 @@ from datetime import datetime
 import ifcopenshell
 from rich.console import Console
 from rich.spinner import Spinner
+from rich.panel import Panel
 
 if __name__ == "__main__":
 
@@ -39,23 +37,31 @@ if __name__ == "__main__":
 
 
     console = Console()
+    console.print("\n")
+    console.print(Panel.fit(
+        "[bold magenta]⭐ IFC ELEMENTLEVELER + FREEHEIGHTCHECKER ⭐[/bold magenta]\n\n[green]Advanced BIM - E25[/green]",
+        title="[bold yellow]BIManalyst TOOL[/bold yellow]",
+        subtitle="by Group 12",
+        border_style="cyan"
+    ))
+    
     # ask for what should be checked
-    elementType = console.input("Please select which element type should be checked (default is IfcDuctSegment): [bold green](IfcDuctSegment/IfcPipeSegment/IfcBeam)[/bold green] ") or "IfcDuctSegment"
+    elementType = console.input("\n\nPlease select which element type should be checked (default is IfcDuctSegment): [bold green](IfcDuctSegment/IfcPipeSegment etc.)[/bold green] ") or "IfcDuctSegment"
 
     # ask if a new ElementLevelChecker.ifc should be saved
-    ELCcolorQuestion = console.input("Do you want a new IFC file, where potentially misplaced elements are colored? [bold green](y/n)[/bold green] ") or "y"
+    ELCcolorQuestion = console.input("\nDo you want a new IFC file, where potentially misplaced elements are colored? (default is yes) [bold green](y/n)[/bold green] ") or "y"
     if ELCcolorQuestion.lower() == 'y':
         ELCcolorQuestion = True
     else:
         ELCcolorQuestion = False
 
     # ask if a new FreeHeightChecker.ifc should be saved
-    FHCcolorQuestion = console.input("Do you want a new IFC file, where the lowest element on each level is colored? [bold green](y/n)[/bold green] ") or "y"
+    FHCcolorQuestion = console.input("\nDo you want a new IFC file, where the lowest element on each level is colored? (default is yes) [bold green](y/n)[/bold green]") or "y"
     if FHCcolorQuestion.lower() == 'y':
         FHCcolorQuestion = True
     else:
         FHCcolorQuestion = False
-
+    
     ########################################################
     #                   TOOL STARTS HERE                   #
     ########################################################
@@ -63,9 +69,11 @@ if __name__ == "__main__":
     ifc_file = ifcopenshell.open(f"ifcFiles/{ifc_fileName}")
 
     # Check element placements in the IFC file
-    with console.status("[bold green]Checking element placements in the IFC file...", spinner='dots'):
+    with console.status("\n[bold green]Checking element placements in the IFC file...", spinner='dots'):
         ifc_fileELC, misplacedElements = ElementLeveler.ElementLevelChecker(ifc_file=ifc_file, elementType=elementType, colorQuestion=ELCcolorQuestion)
-    console.print(f"Total elements potentially misplaced: {len(misplacedElements[0]) + len(misplacedElements[1])} \n")
+    console.print(f"""Total elements potentially misplaced: {len(misplacedElements[0]) + len(misplacedElements[1])} \n
+- Elements potentially placed on the wrong level: {len(misplacedElements[0])} \n
+- Elements placed between levels (Should potentially be moved to building): {len(misplacedElements[1])} \n""")
     if ELCcolorQuestion is True:
         # save the ifc file to desktop
         levelCheckFileName = "ElementLeveler.ifc"
@@ -88,7 +96,13 @@ if __name__ == "__main__":
     dt_string = now.strftime("%Y-%m-%d")
     with open(f"ifcFiles/misplacedElements_REPORT_{dt_string}.txt", "w") as f:
         f.write(f"Misplaced Elements Report - Generated on {dt_string}\n")
-        f.write("========================================\n\n")
+        f.write("========================================\n")
+        f.write(f"""\n Overview of potentially misplaced elements for element type: {elementType}\n
+Total elements potentially misplaced: {len(misplacedElements[0]) + len(misplacedElements[1])}
+- Elements potentially placed on the wrong level: {len(misplacedElements[0])}
+- Elements placed between levels (Should potentially be moved to building): {len(misplacedElements[1])}
+\n========================================\n\n""")
+
         f.write("⚠️ Elements potentially placed on the wrong level ⚠️:\n")
         for element in misplacedElements[0]:
             f.write(f"Element {element['elementID']} (Type: {element['elementType']})\n")
@@ -116,4 +130,4 @@ if __name__ == "__main__":
 
 
 
-    console.print("Done!")
+    console.print("[bold cyan]Done![bold cyan]\n")
